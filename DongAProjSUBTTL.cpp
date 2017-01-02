45b2b709e5b84223e5602195e1b8079b7b4101b7
KENNYSOFT <525hm@naver.com>
2017-01-02 17:44:25 +0900

    v2.0

--- DongAProjSUBTTL.cpp
+++ DongAProjSUBTTL.cpp
@@ -6,10 +6,9 @@ FILE*ffs=fopen("finished.txt","r");
 #define HEIGHT 40
 #define WIDTH 180
 FILE*fin,*fout;
-//char line[45010][10][510];
-char filename[1010],line[10][510];
-wchar_t linew[10][210],delimeter[]=L"¢Ò";
-int avail,cut;
+char filename[1010],line[45010][8][510];
+wchar_t linew[45010][8][210],delimeter[]=L"¢Ò";
+int n,pos,avail,cut,dir=1;
 inline void wprintf(const wchar_t ch)
 {
 	if(wprintf(L"%c",ch)!=1)wprintf(ch<128?L"?":L"£¿");
@@ -23,92 +22,106 @@ void print(int line=0)
 		return;
 	}
 	gotoxy(0,line-1);
-	if(line!=5)for(i=0;linew[line][i]&&wherex()<WIDTH-2;++i)wprintf(linew[line][i]);
+	if(line!=5)for(i=0;linew[pos][line][i]&&wherex()<WIDTH-2;++i)wprintf(linew[pos][line][i]);
 	else
 	{
-		for(i=0;linew[line][i]!='\t';++i)wprintf(linew[line][i]);
+		for(i=0;linew[pos][line][i-1]!='\t';++i)wprintf(linew[pos][line][i]);
 		SetConsoleColor(15,0);
-		for(;i<=cut;++i)wprintf(linew[line][i]);
+		for(;i<=cut&&wherex()<WIDTH-2;++i)wprintf(linew[pos][line][i]);
 		SetConsoleColor(0,15);
-		for(;linew[line][i]&&wherex()<WIDTH-2;++i)wprintf(linew[line][i]);
+		for(;linew[pos][line][i]&&wherex()<WIDTH-2;++i)wprintf(linew[pos][line][i]);
 	}
 	for(i=wherex();i<WIDTH-2;++i)printf(" ");
-	gotoxy(0,7);
+}
+void move()
+{
+	int i;
+	pos=pos+dir;
+	cut=5;
+	if(wcsstr(linew[pos][5],delimeter))// todo: multi delemeter
+	{
+		avail=1;
+		for(i=6;linew[pos][5][i];++i)
+		{
+			if(linew[pos][5][i]==delimeter[0])cut=i;
+			if(i>cut+20)break;
+		}
+	}
+	else if(wcsstr(linew[pos][3]+6,L"#N/A")&&wcslen(linew[pos][7])==7)avail=1;
+	else
+	{
+		avail=0;
+		/*for(i=6;linew[pos][5][i];++i)
+		{
+			if((linew[pos][5][i]>='A'&&linew[pos][5][i]<='Z')||(linew[pos][5][i]>='a'&&linew[pos][5][i]<='z'))
+			{
+				avail=1;
+				cut=5;
+				break;
+			}
+		}*/
+	}
+	if(avail)print();
 }
 void load()
 {
-	int i;
-	for(i=1;i<=7;++i)
-	{
-		fgets(line[i],500,fin);
-		utf8_to_unicode(line[i],linew[i]);
-		if(linew[i][wcslen(linew[i])-1]=='\n')linew[i][wcslen(linew[i])-1]=0;
-	}
-	cut=5;
-	if(wcsstr(linew[5],delimeter))
-	{
-		avail=1;
-		for(i=6;linew[5][i];++i)
-		{
-			if(linew[5][i]==delimeter[0])cut=i;
-			if(i>cut+20)break;
-		}
-		print();
-	}
-	else avail=0;
-	// alphabet include -> xx?
-}
-void save()
-{
-	int i;
-	for(i=1;i<=7;++i)
-	{
-		wcscat(linew[i],L"\n");
-		unicode_to_utf8(linew[i],line[i]);
-		fputs(line[i],fout);
-	}
-	fflush(fout);
-}
-int main()
-{
-	int i,j,k,finished,pos;
-	char ch,ch2;
-	SetConsoleTitle("DongAProjSUBTTL");
-	SetConsoleSize(HEIGHT,WIDTH);
-	system("color f0");
-	setlocale(LC_ALL,"korean");
-	gotoxy(0,39);
-	//printf("¢Õ prev, next");
-	printf("¡é next / ¡ê adjust / [ ]¢Ò commit / x mark / q quit");
-	fgets(filename,1000,ffn);
-	fscanf(ffs,"%d",&finished);
-	fin=fopen(filename,"r");
-	if(fin==NULL)
-	{
-		cout<<"file not exist";
-		return 1;
-	}
-	for(i=strlen(filename)-1;i>=0;--i)if(filename[i]=='.')break;
-	memmove(filename+i+7,filename+i,strlen(filename)-i+1);
-	memcpy(filename+i,"_result",7);
-	fout=fopen(filename,"w");
-	for(i=0;i<=finished;++i)
+	int i,j;
+	for(i=0;;++i)
 	{
 		for(j=1;j<=7;++j)
 		{
-			fgets(line[j],400,fin);
-			fputs(line[j],fout);
+			fgets(line[i][j],500,fin);
+			if(feof(fin))goto end;
+			utf8_to_unicode(line[i][j],linew[i][j]);
+			if(linew[i][j][wcslen(linew[i][j])-1]=='\n')linew[i][j][wcslen(linew[i][j])-1]=0;
+		}
+		wcscpy(linew[i][0],linew[i][4]);
+	}
+end:
+	n=i-1;
+}
+void save()
+{
+	int i,j;
+	char command[1024];
+	sprintf(command,"copy %s %s.bak >nul",filename,filename);
+	system(command);
+	fout=fopen(filename,"w");
+	for(i=0;i<=n;++i)
+	{
+		for(j=1;j<=7;++j)
+		{
+			unicode_to_utf8(linew[i][j],line[i][j]);
+			strcat(line[i][j],"\n");
+			fputs(line[i][j],fout);
 		}
 	}
+	fclose(fout);
+}
+int main()
+{
+	char ch,ch2;
+	SetConsoleTitle("DongAProjSUBTTL");
+	SetConsoleSize(HEIGHT,WIDTH);
+	SetCursorType(NOCURSOR);
+	system("color f0");
+	setlocale(LC_ALL,"korean");
+	gotoxy(0,HEIGHT-1);
+	wprintf(L"¢Õ move / ¡ê adjust / [ ]¢Ò commit / r reset / x mark / s save / q quit");
+	fgets(filename,1000,ffn);
+	fscanf(ffs,"%d",&pos);
+	fin=fopen(filename,"r");
+	if(fin==NULL)
+	{
+		wprintf(L"file not exist");
+		return 1;
+	}
 	load();
+	fclose(fin);
+	move();
 	while(1)
 	{
-		if(!avail)
-		{
-			save();
-			load();
-			continue;
-		}
+		while(!avail)move();
 		ch=getch();
 		switch(ch)
 		{
@@ -117,32 +130,55 @@ int main()
 			switch(ch2)
 			{
 			case LEFT:
-				if(cut>5)for(cut--;linew[5][cut]!=delimeter[0]&&cut>5;--cut);
+				if(cut>5)for(cut--;linew[pos][5][cut]!=delimeter[0]&&cut>5;--cut);
 				print(5);
 				break;
 			case RIGHT:
-				if(cut<wcslen(linew[5])-1)for(cut++;linew[5][cut]!=delimeter[0]&&linew[5][cut+1];++cut);
+				if(cut<wcslen(linew[pos][5])-1)for(cut++;linew[pos][5][cut]!=delimeter[0]&&linew[pos][5][cut+1];++cut);
 				print(5);
 				break;
+			case UP:
+				dir=-1;
+				move();
+				break;
 			case DOWN:
-				save();
-				load();
+				dir=1;
+				move();
 				break;
 			}
 			break;
 		case ' ':
 		case ENTER:
-			wcsncat(linew[4],linew[5]+6,cut-5);
-			memmove(linew[5]+6,linew[5]+cut+1,sizeof(wchar_t)*(wcslen(linew[5])-cut));
-			cut=5;
+			if(cut>5)
+			{
+				wcsncat(linew[pos][4],linew[pos][5]+6,cut-5);
+				memmove(linew[pos][5]+6,linew[pos][5]+cut+1,sizeof(wchar_t)*(wcslen(linew[pos][5])+1-cut));
+				cut=5;
+				print(4);
+				print(5);
+			}
+			break;
+		case 'r':
+		case 'R':
+			memmove(linew[pos][5]+6+(wcslen(linew[pos][4])-wcslen(linew[pos][0])),linew[pos][5]+6,sizeof(wchar_t)*(wcslen(linew[pos][5])+1-6));
+			memcpy(linew[pos][5]+6,linew[pos][4]+7,sizeof(wchar_t)*(wcslen(linew[pos][4])-wcslen(linew[pos][0])));
+			wcscpy(linew[pos][4],linew[pos][0]);
 			print(4);
 			print(5);
 			break;
 		case 'x':
 		case 'X':
-			if(wcslen(linew[7])==7)wcscat(linew[7],L"xx");
-			else linew[7][7]=0;
+			if(wcslen(linew[pos][7])==7)wcscat(linew[pos][7],L"xx");
+			else linew[pos][7][7]=0;
 			print(7);
+			break;
+		case 's':
+		case 'S':
+			gotoxy(0,HEIGHT-2);
+			wprintf(L"Saving...");
+			save();
+			gotoxy(0,HEIGHT-2);
+			wprintf(L"         ");
 			break;
 		case CTRLC:
 		case 'q':
